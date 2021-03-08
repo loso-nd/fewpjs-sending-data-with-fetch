51ea8b9dc929ee4537aa44fd26631e3a11941e6b
Nahum <losond8@gmail.com>
2021-03-08 18:43:44 -0500

    Sending Data with Fetch - Understanding how to directly append data retrieved for the server to the DOM

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
 // Add your code here
-function init (){
-    fetch('http://localhost:3000/dogs', {
-        method: "POST",
-        headers: {
-//tells the server the type format we accept, typically implied
-                "Content-Type" : "application/json", 
-                "Accept": "application/json"
-        },
-        body:
-//request needs to be a string, The object, converted to JSON 
-//often send multiple pieces of information in one request. In our code, we often organize this information using objs.    
-//Using JSON has enabled us to preserve the key/value pairs of our object w/in the string.    
-            JSON.stringify({
-            dogName: "Byron",
-            dogBreed: "Poodle"
-        })
-    })
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+
+const submitData = () => {
+
+  return  fetch(`http://localhost:3000/users`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com"
+  })
+})
+ .then(response => response.json())
+ .then(json => renderToDom(json))
+ .catch(function(error) {
+
+    document.body.innerHTML = error
+  })
+
+//   .catch( function ( error ) {
+//     // display error message in the body instead of id
+//   } )
+}
+submitData()
+
+function renderToDom(json) {
+    document.body.innerHTML = json["id"]
 }
 
 
-function submitData(name, email){
+// function renderToDom(json) {
+//     document.body.innerHTML = json.id
+//   // or document.body.innerHTML = json["id"]
+// }
 
-    fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: {
-//tells the server the type format we accept, typically implied
-                "Content-Type" : "application/json", 
-                "Accept": "application/json"
-        },
-        body:
-            JSON.stringify({
-                name: "Getting Started",
-                email: "catchmeoutside@outchyea.yee"
-            })
-        .then(resp => resp.json())
-        .then(newUser => console.log(newUser))
-        .catch(error => console.log(error))
-}
 
-    )}
-console.log(init)
-document.addEventListener("DOMContentLoaded", init);
+//     fetch('http://localhost:3000/dogs', {
+//         method: "POST",
+//         headers: {
+// //tells the server the type format we accept, typically implied
+//                 "Content-Type" : "application/json", 
+//                 "Accept": "application/json"
+//         },
+//         body:
+// //request needs to be a string, The object, converted to JSON 
+// //often send multiple pieces of information in one request. In our code, we often organize this information using objs.    
+// //Using JSON has enabled us to preserve the key/value pairs of our object w/in the string.    
+//             JSON.stringify({
+//             dogName: "Byron",
+//             dogBreed: "Poodle"
+//         })
+//     })
+//     .then(resp => resp.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error))
+// }
+
